d8d133239bf38a1485640c59ce1aa66ef23b21cb
Gabriel <gabrielmend.sousa@gmail.com>
2025-02-15 14:20:59 -0300

    final

--- appp.js
+++ appp.js
@@ -47,7 +47,6 @@ function exibirAmigos(){
 
 
 function sortearAmigos() {
-    // Verifica se a lista está vazia
     if (listaDeAmigos.length === 0) {
         exibirConflitos("h2", "Não há amigos para serem sorteados");
         return; // Sai da função se não houver amigos
@@ -62,25 +61,22 @@ function sortearAmigos() {
             return;
         }
 
-        // Sorteia um índice aleatório
+        
         let indiceSorteado = Math.floor(Math.random() * listaDeAmigos.length);
         let nomeSorteado = listaDeAmigos[indiceSorteado];
 
-        // Exibe o nome sorteado
+        
         exibirResultado(nomeSorteado);
 
         // Remove o amigo sorteado da lista
         listaDeAmigos.splice(indiceSorteado, 1);
 
-        // Atualiza a lista de amigos na tela
+        
         exibirAmigos();
 
-        // Sorteia o próximo amigo após um intervalo de tempo
-        setTimeout(sortearProximo, 2000); // 2000ms = 2 segundos
+        
     }
 
-    // Inicia o sorteio
-    sortearProximo();
 }
 
 
